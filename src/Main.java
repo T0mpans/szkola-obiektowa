0f6dc638b42be84346f20d5bad86452e9210c5c6
T0mpans <19tomek11@gmail.com>
2025-10-27 14:56:48 +0100

    commit6

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,19 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        Osoba osoba = new Osoba();
+        Osoba osoba = new Uczen();
         System.out.println(osoba.imie);
         osoba.imie="Jaś";
         System.out.println(osoba.imie);
+        osoba.setWiek(12);
+        System.out.println("wiek: "+osoba.getWiek());
+        System.out.println(osoba);
+        System.out.println("Liczba uczniów: "+Uczen.getLiczbaUczniow());
+        Uczen uczen = new Uczen("Jadwiga", 16, 123);
+        Uczen uczen2 = new Uczen("Janush", 17, 1234);
+        System.out.println("Liczba uczniów: "+Uczen.getLiczbaUczniow());
+        System.out.println(uczen);
+        System.out.println(uczen2);
+
     }
 }
